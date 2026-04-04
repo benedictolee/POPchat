fdbf09ccd9a37db1c11694d7c177a1c6cffb1f5d
lee jae kwon <93851595+benedictolee@users.noreply.github.com>
2026-04-04 15:51:01 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'POPchat',
-  description: '한 화면에서 여러 질문을 해보세요. 팝업 서브 채팅 지원.'
+  description: '한 화면에서 여러 질문을 해보세요. 팝업 서브 채팅 지원.', 
   icons: { apple: '/pop-icon-192.png' },
   verification: {
     google: 'GF572f_q5kWMiUk88gxJJTkj1B5ZO99Nmvccp1-v46o',
